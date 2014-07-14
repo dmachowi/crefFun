f3abbc8d74d8b17fcc4d915d6a2a899e32280290
Drew Machowicz <drewmachowicz@macbook.lan>
2014-07-14 21:56:08 +0200

    Initial Commit

---
+++ crefFun/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  crefFun
+//
+//  Created by Drew Machowicz on 7/14/14.
+//  Copyright (c) 2014 Drew Machowicz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
